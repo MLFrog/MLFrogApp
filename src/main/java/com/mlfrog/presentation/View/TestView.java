b839ecd95294df84b9c7c6f04bb183ae6588a969
CASY82 <repaid11010@gmail.com>
2023-04-01 12:20:36 +0900

    add : 20230401 - test code

--- src/main/java/com/mlfrog/presentation/View/TestView.java
+++ src/main/java/com/mlfrog/presentation/View/TestView.java
@@ -1,7 +1,13 @@
 package com.mlfrog.presentation.View;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Test View Controller 입니다.
+ * 추후 변경되거나 없어질 부분입니다.
+ */
 
 @RestController
 public class TestView {
@@ -10,4 +16,10 @@ public class TestView {
 	public String mainPage() {
 		return "Main 입니다";
 	}
+	
+	@RequestMapping("/{id}")
+	public String idTest(@PathVariable int id) {
+		return Integer.toString(id);
+	}
+	
 }
